6ffdfdf7c97045539050609ffb2a187ce0519ed3
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-02 14:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function removeFromCart(item) {
   } else {
     for ( var key in cart ) {
       if ( item === key ) {
-          cart.splice(0, 1);
+          delete cart[item];
 
           return cart;
       }
